4fe2697a2324956115b5a9e7b2a696c1e9da1740
zlodiak <prozaik81-2@yandex.ru>
2016-04-30 14:17:22 +0300

    n

--- head.php
+++ head.php
@@ -3,7 +3,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <!-- <link rel="icon" href="../../favicon.ico"> -->
 
-    <title>graphKalinin</title>
+    <title>shippingCalculator1</title>
 
     <!-- Bootstrap core CSS -->
     <link href="js/bootstrap-3/css/bootstrap.min.css" rel="stylesheet">
